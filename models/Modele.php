c9ece7375bf4899d5d6e441066e567f9eace7e0b
Taupin Jérémy <jeremytaupin@hotmail.fr>
2021-05-17 12:39:34 +0200

    require once config.php

--- models/Modele.php
+++ models/Modele.php
@@ -1,14 +1,13 @@
 <?php
 
 
+
+
 trait Modele {
   private $pdo = null;
   public function __construct() {
     try {
-      $HOST = "3.15.45.226:3306";
-      $DATABASE_NAME = "dbmentorat";
-      $USERNAME = "phpmentorat";
-      $PASSWORD = "RoroIsHere@3326";
+      require_once('./models/config.php');
       $this->pdo = new PDO("mysql:host=$HOST; dbname=$DATABASE_NAME", $USERNAME, $PASSWORD);
       $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $err) {
